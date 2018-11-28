6f6977ba66b2b9aa9e4ac115bd42a82006689e89
Pavel Efimenko <p.efimenko@pragmaspace.com>
2018-11-28 17:26:20 +0200

    initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtendedDefineWebpackPlugin = require('extended-define-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/env", "@babel/react"]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtendedDefineWebpackPlugin({
+            __INTERNAL_API_URL__: process.env.INTERNAL_API_URL || 'http://localhost:5000'
+        }),
+        new CopyWebpackPlugin([
+            {from: 'assets/images', to: 'assets/images'}
+        ])
+    ],
+    devtool: 'source-map',
+    devServer: {
+        port: process.env.PORT || 4000
+    }
+};
