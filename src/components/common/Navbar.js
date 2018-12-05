7ee4e79ad8f01836d30a4ce0fb4bb69baf5a1681
Pavel Efimenko <p.efimenko@pragmaspace.com>
2018-12-05 18:19:43 +0200

    WIP: navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -3,8 +3,22 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <div className="footer">
-                Navbar
+            <div className="navbar">
+                <div className="navbar__logo-container">
+                    <div className="navbar__logo-container__img">
+                        <img src="../../../assets/images/foottrace.png" alt="Logo"/>
+                    </div>
+                </div>
+                <div className="navbar__menu">
+                    <div className="navbar__menu__item">
+                        <div className="navbar__menu__item__icon">
+                            Icon
+                        </div>
+                        <div className="navbar__menu__item__title">
+                            Мыши
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
