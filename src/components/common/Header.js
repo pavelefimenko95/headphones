b50e916b7a0764a719f5a4d1fa1b969eda61289e
Pavel <pavel.efimen95@gmail.com>
2019-03-02 17:09:35 +0300

    made navbar open on click on mobile devices

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaCircle } from "react-icons/fa";
 import ReactSVG from 'react-svg';
+import classNames from 'classnames';
+import onOutsideElementClick from '../../utils/onOutsideElementClick';
 import * as productConstants from '../../constants/product';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpened: false
+        };
+
+        this.openHeader = this.openHeader.bind(this);
+        this.onLinkClick = this.onLinkClick.bind(this);
+    }
+
     render() {
+        let { state } = this;
+
         return (
-            <div className="header">
+            <div
+                className={ classNames('header', {'header--opened': state.isOpened }) }
+                onClick={ this.openHeader }
+                ref={el => this.headerRef = el}
+            >
                 <div className="header__logo-container">
                     <div className="header__logo-container__img">
                         <ReactSVG src="../../../assets/images/logo.svg" />
                     </div>
                 </div>
                 <div className="header__menu">
-                    <NavLink to={`/products/${ productConstants.MOUSE.toLowerCase() }`} className="header__menu__item">
+                    <NavLink
+                        to={`/products/${ productConstants.MOUSE.toLowerCase() }`}
+                        className="header__menu__item"
+                        onClick={ this.onLinkClick }
+                    >
                         <div className="header__menu__item__icon">
                             <ReactSVG svgClassName="header__menu__item__icon__svg" src="../../../assets/images/mouse-icon.svg" />
                         </div>
@@ -22,7 +45,11 @@ class Header extends Component {
                             <span>мыши</span>
                         </div>
                     </NavLink>
-                    <NavLink to={`/products/${ productConstants.KEYBOARD.toLowerCase() }`} className="header__menu__item">
+                    <NavLink
+                        to={`/products/${ productConstants.KEYBOARD.toLowerCase() }`}
+                        className="header__menu__item"
+                        onClick={ this.onLinkClick }
+                    >
                         <div className="header__menu__item__icon">
                             <ReactSVG svgClassName="header__menu__item__icon__svg" src="../../../assets/images/keyboard.svg" />
                         </div>
@@ -30,7 +57,11 @@ class Header extends Component {
                             <span>клавиатуры</span>
                         </div>
                     </NavLink>
-                    <NavLink to={`/products/${ productConstants.HEADPHONES.toLowerCase() }`}  className="header__menu__item">
+                    <NavLink
+                        to={`/products/${ productConstants.HEADPHONES.toLowerCase() }`}
+                        className="header__menu__item"
+                        onClick={ this.onLinkClick }
+                    >
                         <div className="header__menu__item__icon">
                             <ReactSVG svgClassName="header__menu__item__icon__svg" src="../../../assets/images/headphones-icon.svg" />
                         </div>
@@ -38,14 +69,14 @@ class Header extends Component {
                             <span>наушники</span>
                         </div>
                     </NavLink>
-                    <div className="header__menu__item">
-                        <div className="header__menu__item__icon">
-                            <ReactSVG svgClassName="header__menu__item__icon__svg" src="../../../assets/images/plus-icon.svg" />
-                        </div>
-                        <div className="header__menu__item__title">
-                            <span>аксуссуары</span>
-                        </div>
-                    </div>
+                    {/*<div className="header__menu__item">*/}
+                        {/*<div className="header__menu__item__icon">*/}
+                            {/*<ReactSVG svgClassName="header__menu__item__icon__svg" src="../../../assets/images/plus-icon.svg" />*/}
+                        {/*</div>*/}
+                        {/*<div className="header__menu__item__title">*/}
+                            {/*<span>аксуссуары</span>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
                     <div className="header__menu__separator">
                     </div>
                     <div className="header__menu__item">
@@ -53,52 +84,78 @@ class Header extends Component {
                             <FaCircle size={ 6 } />
                         </div>
                         <div className="header__menu__item__title">
-                            <span>о bloody</span>
+                            <span>о нас</span>
                         </div>
                     </div>
-                    <div className="header__menu__item">
-                        <div className="header__menu__item__icon header__menu__item__icon--circle">
-                            <FaCircle size={ 6 } />
-                        </div>
-                        <div className="header__menu__item__title">
-                            <span>запрос</span>
-                        </div>
-                    </div>
-                    <div className="header__menu__item">
-                        <div className="header__menu__item__icon header__menu__item__icon--circle">
-                            <FaCircle size={ 6 } />
-                        </div>
-                        <div className="header__menu__item__title">
-                            <span>пресс-центр</span>
-                        </div>
-                    </div>
-                    <div className="header__menu__item">
-                        <div className="header__menu__item__icon header__menu__item__icon--circle">
-                            <FaCircle size={ 6 } />
-                        </div>
-                        <div className="header__menu__item__title">
-                            <span>галерея</span>
-                        </div>
-                    </div>
-                    <div className="header__menu__item">
-                        <div className="header__menu__item__icon header__menu__item__icon--circle">
-                            <FaCircle size={ 6 } />
-                        </div>
-                        <div className="header__menu__item__title">
-                            <span>скачать</span>
-                        </div>
-                    </div>
-                    <div className="header__menu__item">
-                        <div className="header__menu__item__icon header__menu__item__icon--circle">
-                            <FaCircle size={ 6 } />
-                        </div>
-                        <div className="header__menu__item__title">
-                            <span>магазин</span>
-                        </div>
-                    </div>
+                    {/*<div className="header__menu__item">*/}
+                        {/*<div className="header__menu__item__icon header__menu__item__icon--circle">*/}
+                            {/*<FaCircle size={ 6 } />*/}
+                        {/*</div>*/}
+                        {/*<div className="header__menu__item__title">*/}
+                            {/*<span>запрос</span>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                    {/*<div className="header__menu__item">*/}
+                        {/*<div className="header__menu__item__icon header__menu__item__icon--circle">*/}
+                            {/*<FaCircle size={ 6 } />*/}
+                        {/*</div>*/}
+                        {/*<div className="header__menu__item__title">*/}
+                            {/*<span>пресс-центр</span>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                    {/*<div className="header__menu__item">*/}
+                        {/*<div className="header__menu__item__icon header__menu__item__icon--circle">*/}
+                            {/*<FaCircle size={ 6 } />*/}
+                        {/*</div>*/}
+                        {/*<div className="header__menu__item__title">*/}
+                            {/*<span>галерея</span>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                    {/*<div className="header__menu__item">*/}
+                        {/*<div className="header__menu__item__icon header__menu__item__icon--circle">*/}
+                            {/*<FaCircle size={ 6 } />*/}
+                        {/*</div>*/}
+                        {/*<div className="header__menu__item__title">*/}
+                            {/*<span>скачать</span>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
+                    {/*<div className="header__menu__item">*/}
+                        {/*<div className="header__menu__item__icon header__menu__item__icon--circle">*/}
+                            {/*<FaCircle size={ 6 } />*/}
+                        {/*</div>*/}
+                        {/*<div className="header__menu__item__title">*/}
+                            {/*<span>магазин</span>*/}
+                        {/*</div>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+        onOutsideElementClick(this.headerRef, () => {
+            this.setState({
+                isOpened: false
+            });
+        });
+    }
+
+    openHeader() {
+        if(!this.state.isOpened) {
+            this.setState({
+                isOpened: true
+            });
+        }
+    }
+
+    onLinkClick(e) {
+        if(!this.state.isOpened && window.screen.width < 550) {
+            e.preventDefault();
+        } else {
+            this.setState({
+                isOpened: false
+            });
+        }
     }
 }
 
